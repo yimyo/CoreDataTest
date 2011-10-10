07a500faa676d0cfa12c9825d7b31ce803cdab09
yimyo Chen <yimyimyoyo@gmail.com>
2011-10-10 12:52:16 +0800

    Initial Commit

---
+++ XMLUse/XMLUseAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  XMLUseAppDelegate.h
+//  XMLUse
+//
+//  Created by yimyo on 2011/10/10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMLUseAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
